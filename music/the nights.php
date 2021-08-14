d536a301ca24e912b48371e8b0eb382245dacb2e
longtc9c2000 <65326062+longtc9c2000@users.noreply.github.com>
2021-08-14 20:35:04 +0700

    lol

---
+++ music/the nights.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>The Nights</title>
+    <?php include("../layout/head.html");?>
+</head>
+<body>
+      <?php include("../layout/header.html");?>
+<section class="content">
+    <div class="container">
+        <h2>The Nights</h2>
+        <div>
+    </div>
+</section>
+    <?php include("../layout/footer");?>
+</body>
+</html>
